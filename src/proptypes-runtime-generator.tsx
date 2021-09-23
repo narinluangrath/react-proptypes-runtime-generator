97b451ee0bc83b1015afa80968b12e087c204bc9
Narin Luangrath <narin.luangrath@gmail.com>
2021-09-22 22:01:34 -0700

    more shit

--- src/proptypes-runtime-generator.tsx
+++ src/proptypes-runtime-generator.tsx
@@ -1,27 +1,47 @@
-import * as React from 'react';
-import { omit } from 'lodash';
+import * as React from "react";
+import { omit } from "lodash";
 
-import { ReactFiberRecur } from './react-fiber-recur';
-import { ObjectDatabase } from './object-database';
-import { getPropType } from './get-proptype';
-import { Shape } from './types';
+import { getNameFromFiber } from "./get-name-from-fiber";
+import { ReactFiberRecur } from "./react-fiber-recur";
+import { ObjectDatabase } from "./object-database";
+import { getPropType } from "./get-proptype";
+import { PropType, Shape } from "./types";
+
+type Statistics = {
+  numInstances: number;
+  componentNames: Set<string>;
+};
+
+const initStats = () => ({
+  numInstances: 0,
+  componentNames: new Set<string>(),
+});
 
 // @ts-expect-error
 export const PropTypesRuntimeGenerator = ({ children }) => {
   React.useEffect(() => {
     // @ts-expect-error
     window.ZOOMZOOM = () => {
+      const stats = new Map<PropType, Statistics>();
       const objDatabase = new ObjectDatabase<Shape>();
       ReactFiberRecur(children._owner, (node) => {
         const props = omit(node.memoizedProps ?? {}, children);
         Object.entries(props).forEach(([, propValue]) => {
-          getPropType(propValue, objDatabase);
-        })
-      })
+          const propType = getPropType(propValue, objDatabase);
 
-      console.log(objDatabase)
-    }
-  }, [children])
+          if (!stats.get(propType)) {
+            stats.set(propType, initStats());
+          }
+
+          stats.get(propType)!.numInstances += 1;
+          stats.get(propType)!.componentNames.add(getNameFromFiber(node));
+        });
+      });
+
+      console.log(objDatabase);
+      console.log(stats);
+    };
+  }, [children]);
 
   return children;
-}
+};
