5c44ab8dc01ba7a32f1ae3cc8021eba445cdd033
Narin Luangrath <narin.luangrath@gmail.com>
2021-09-24 20:31:12 -0700

    chore: add some types yo

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,19 @@
 export type PropType = string;
 export type Shape = { [key: string]: PropType };
+
+type Component = {
+  componentName: string;
+  fileName: string;
+};
+
+export type ExportedPropTypeData = {
+  propType: PropType;
+  associatedpropNames: string[];
+  associatedComponents: Component[];
+}[];
+
+export type ExportedComponentData = {
+  component: Component;
+  props: { [propName: string]: PropType };
+  exampleData: { [propName: string]: any };
+}[];
