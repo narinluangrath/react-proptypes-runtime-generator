543b464e25399a763c2bab647c3dc1c88ac0ab9e
Narin Luangrath <narin.luangrath@gmail.com>
2021-10-03 17:27:06 -0700

    test: add test for traverse-dom-bfs

--- src/traverse-dom-bfs.test.tsx
+++ src/traverse-dom-bfs.test.tsx
@@ -1,30 +1,42 @@
 import { traverseDomBfs } from "./traverse-dom-bfs";
 
 describe("traverse-dom-bfs", () => {
+  const one = document.createElement("div");
+  one.id = "1";
+
+  const two = document.createElement("div");
+  two.id = "2";
+
+  const three = document.createElement("div");
+  three.id = "3";
+
+  const four = document.createElement("div");
+  four.id = "4";
+  one.appendChild(four);
+
+  document.body.appendChild(one);
+  document.body.appendChild(two);
+  document.body.appendChild(three);
+  document.body.id = "0";
+
   it("traverses the dom in breadth first search order", () => {
-    const one = document.createElement("div");
-    one.id = "1";
-
-    const two = document.createElement("div");
-    two.id = "2";
-
-    const three = document.createElement("div");
-    three.id = "3";
-
-    const four = document.createElement("div");
-    four.id = "4";
-    one.appendChild(four);
-
-    document.body.appendChild(one);
-    document.body.appendChild(two);
-    document.body.appendChild(three);
-    document.body.id = "0";
-
     const ids: string[] = [];
     traverseDomBfs(document.body, (node) => {
       ids.push(node.id);
     });
 
     expect(ids).toEqual(["0", "1", "2", "3", "4"]);
+  });
+
+  it("stops when callback returns true", () => {
+    const ids: string[] = [];
+    traverseDomBfs(document.body, (node) => {
+      ids.push(node.id);
+      if (node.id === "2") {
+        return true;
+      }
+    });
+
+    expect(ids).toEqual(["0", "1", "2"]);
   });
 });
