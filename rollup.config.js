f42f6aa2e69147f43078dedbeaf1fa86cc40b770
Narin Luangrath <narin.luangrath@gmail.com>
2021-10-10 21:41:28 -0700

    build: no umd

--- rollup.config.js
+++ rollup.config.js
@@ -3,44 +3,22 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import autoExternal from 'rollup-plugin-auto-external';
 import { babel } from '@rollup/plugin-babel';
 import pkg from './package.json';
 
 export default [
-	// browser-friendly UMD build
 	{
 		input: 'src/index.ts',
-		output: {
-			name: 'reactProptypesRuntimeGenerator',
-			file: pkg.browser,
-			format: 'umd'
-		},
 		plugins: [
-			nodeResolve(),   // so Rollup can find `ms`
-			commonjs(),      // so Rollup can convert `ms` to an ES module
+			nodeResolve(),   // so Rollup can find node modules
+			commonjs(),      // so Rollup can convert node modules to an ES modules
 			typescript(),    // so Rollup can convert TypeScript to JavaScript
 			babel({ babelHelpers: 'bundled' }),
-      autoExternal(),
-		]
-	},
-
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify 
-	// `file` and `format` for each target)
-	{
-		input: 'src/index.ts',
-		plugins: [
-			typescript(), // so Rollup can convert TypeScript to JavaScript
-			babel({ babelHelpers: 'bundled' }),
-      autoExternal(),
 		],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' }
-		]
+		],
+		external: ['react'],
 	}
 ];
