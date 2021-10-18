d137abd3d1b3b42f6366dba094cb206d70805fca
Narin Luangrath <narin.luangrath@gmail.com>
2021-10-18 14:31:48 -0700

    whatever

--- rollup.config.js
+++ rollup.config.js
@@ -21,6 +21,20 @@ export default [
 		],
 		external: [],
 	},
+	// Post install
+	{
+		input: 'src/post-install.ts',
+		plugins: [
+			nodeResolve(),   // so Rollup can find node modules
+			commonjs(),      // so Rollup can convert node modules to an ES modules
+			typescript(),    // so Rollup can convert TypeScript to JavaScript
+			babel({ babelHelpers: 'inline' }),
+		],
+		output: [
+			{ file: pkg.bin['rprg-post-install'], format: 'cjs', banner: '#!/usr/bin/env node' },
+		],
+		external: [],
+	},
 	// ETL
 	{
 		input: 'src/data-etl.ts',
