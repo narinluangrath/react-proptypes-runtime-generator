63b46589aad7d6d90d13ce2c536a5d34c6f8919f
Narin Luangrath <narin.luangrath@gmail.com>
2021-10-19 21:59:09 -0700

    make it better

--- src/backend/post-install.ts
+++ src/backend/post-install.ts
@@ -1,78 +1,98 @@
-import globby from "globby";
-
 import util from "util";
 import fs from "fs";
 // import path from "path";
+
+import globby from "globby";
 import reactDocs from "react-docgen";
 import { sync as pkgDirSync } from "pkg-dir";
-
-const pkgDir = pkgDirSync();
-if (!pkgDir) {
-  console.error("Failed to find root package directory.");
-}
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 // const rprgDir = path.resolve(pkgDir!, "./.rprg");
 
-async function main(pattern = "**/*.{js,jsx,ts,tsx}") {
-  const files = await globby(pattern);
+const handleError = (e: Error, message: string) => {
+  console.error(message);
+  console.error(e);
+  process.exit(1);
+};
 
-  const reactDocsData = [];
+async function writeComponentMap(pattern = "**/*.{js,jsx,ts,tsx}") {
+  const reactDocsData = {};
+  const files = await globby(pattern);
   for (const file of files) {
     try {
-      const data = await readFile(file);
-      reactDocs
-        .parse(data, reactDocs.resolver.findAllExportedComponentDefinitions)
-        .forEach(({ displayName, props }) =>
-          reactDocsData.push({ displayName, props, file })
-        );
+      const fileData = await readFile(file);
+      const componentDefs = reactDocs.parse(
+        fileData,
+        reactDocs.resolver.findAllExportedComponentDefinitions
+      );
+      if (componentDefs.length) {
+        reactDocsData[file] = componentDefs;
+      }
     } catch (error) {
-      console.error("Failed to read file");
-      console.error(error);
-      process.exit(1);
+      handleError(error, `Failed to read file ${file}`);
     }
   }
 
-  const theImports = reactDocsData
-    .map(
-      ({ file }, i) =>
-        `import DefaultExport${i}, * as NamedExports${i} from '${file}';`
-    )
-    .join("\n");
-
-  const theFunctionDeclaration = `function registerExports(defaultExport, namedExports, file, id) {
+  const stringified = JSON.stringify(reactDocsData, null, 2);
   try {
-    defaultExport.__filename = file;
-    defaultExport.__id = id;
-    Object.keys(namedExports).forEach(key => {
-      namedExports[key].__filename = file;
-      namedExports[key].__id = id;
+    await writeFile("./component-map.js", `export default ${stringified}`);
+    return reactDocsData;
+  } catch (error) {
+    handleError(error, "Failed to write component-map.js file");
+  }
+}
+
+async function writeRegisterFunctions(componentMap) {
+  const registerFunctions = `import componentMap from './component-map';
+${Object.keys(componentMap)
+  .map(
+    (file, i) =>
+      `import DefaultExport${i}, * as NamedExports${i} from '${file}';`
+  )
+  .join("\n")}
+
+function registerExports(defaultExport, namedExports, file) {
+  const displayNames = componentMap[file].map(({ displayName }) => displayName);
+  try {
+    if (displayNames.includes(defaultExport.displayName)) {
+      defaultExport.__filename = file;
+      defaultExport.__exportPosition = 'default';
+    }
+    Object.keys(namedExports).forEach((key, exportPosition) => {
+      if (displayNames.includes(namedExports[key].displayName)) {
+        namedExports[key].__filename = file;
+        namedExports[key].__exportPosition = exportPosition;
+      }
     });
   } catch (e) {
     console.warning('Failed to modify React component for tracking purposes');
     console.warning(e);
   }
-}`;
 
-  const theFunctionCalls = reactDocsData
-    .map(
-      ({ file }, i) =>
-        `registerExports(DefaultExport${i}, NamedExports${i}, ${file}, ${i})`
-    )
-    .join("\n");
-
+${Object.keys(componentMap)
+  .map(
+    (file, i) =>
+      `registerExports(DefaultExport${i}, NamedExports${i}, ${file});`
+  )
+  .join("\n")}
+`;
   try {
-    await writeFile(
-      "register-functions.js",
-      [theImports, theFunctionDeclaration, theFunctionCalls].join("\n\n")
-    );
+    await writeFile("register-functions.js", registerFunctions);
   } catch (error) {
-    console.error("Failed to write register-functions.js");
-    console.error(error);
-    process.exit(1);
+    handleError(error, "Failed to write register-functions.js");
   }
+}
+
+async function main() {
+  const pkgDir = pkgDirSync();
+  if (!pkgDir) {
+    console.error("Failed to find root package directory.");
+  }
+
+  const componentMap = await writeComponentMap();
+  await writeRegisterFunctions(componentMap);
 }
 
 main();
