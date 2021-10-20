f16face1a56de2699569bb00dbf4f54e395d077c
Narin Luangrath <narin.luangrath@gmail.com>
2021-10-19 20:48:11 -0700

    add post-install script

--- src/backend/post-install.ts
+++ src/backend/post-install.ts
@@ -1,8 +1,9 @@
 import globby from "globby";
 
+import util from "util";
 import fs from "fs";
-import path from "path";
-import reactDocs from 'react-docgen';
+// import path from "path";
+import reactDocs from "react-docgen";
 import { sync as pkgDirSync } from "pkg-dir";
 
 const pkgDir = pkgDirSync();
@@ -10,45 +11,68 @@ if (!pkgDir) {
   console.error("Failed to find root package directory.");
 }
 
-const rprgDir = path.resolve(pkgDir!, "./.rprg");
-fs.mkdirSync(rprgDir);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+// const rprgDir = path.resolve(pkgDir!, "./.rprg");
 
 async function main(pattern = "**/*.{js,jsx,ts,tsx}") {
-  const reactDocsData = [];
   const files = await globby(pattern);
-  files.forEach(file => {
-    fs.readFile(file, (error, data) => {
-      if (error) {
-        console.error('Failed to read file')
-        console.error(error);
-        process.exit(1);
-      } else {
-        const { displayName, props } = reactDocs.parse(data, reactDocs.resolver.findAllExportedComponentDefinitions);
-        reactDocsData.push({ displayName, props, file });
-      }
-    })
-  });
+
+  const reactDocsData = [];
+  for (const file of files) {
+    try {
+      const data = await readFile(file);
+      reactDocs
+        .parse(data, reactDocs.resolver.findAllExportedComponentDefinitions)
+        .forEach(({ displayName, props }) =>
+          reactDocsData.push({ displayName, props, file })
+        );
+    } catch (error) {
+      console.error("Failed to read file");
+      console.error(error);
+      process.exit(1);
+    }
+  }
 
   const theImports = reactDocsData
-    .map(({ file }, i) => `import DefaultExport${i}, * as NamedExports${i} from '${file}';`)
+    .map(
+      ({ file }, i) =>
+        `import DefaultExport${i}, * as NamedExports${i} from '${file}';`
+    )
     .join("\n");
 
-  const theTryCatch = reactDocsData
-    .map(({ file, i }) => `
-      try {
-        DefaultExport${i}.__filename = '${file}';
-        Object.keys(NamedExports${i}).forEach(key => {
-          NamedExports[key].__filename = '${file}';
-        });
-      } catch (e) {
-        console.warning('Failed to modify React component for tracking purposes');
-        console.warning(e);
-      }
-    `)
-    .join("\n")
+  const theFunctionDeclaration = `function registerExports(defaultExport, namedExports, file, id) {
+  try {
+    defaultExport.__filename = file;
+    defaultExport.__id = id;
+    Object.keys(namedExports).forEach(key => {
+      namedExports[key].__filename = file;
+      namedExports[key].__id = id;
+    });
+  } catch (e) {
+    console.warning('Failed to modify React component for tracking purposes');
+    console.warning(e);
+  }
+}`;
 
-  fs.writeFile('allTheImports.js', `${theImports}\n\n${theTryCatch}\n`, (err) => {
-    
-  })
+  const theFunctionCalls = reactDocsData
+    .map(
+      ({ file }, i) =>
+        `registerExports(DefaultExport${i}, NamedExports${i}, ${file}, ${i})`
+    )
+    .join("\n");
+
+  try {
+    await writeFile(
+      "register-functions.js",
+      [theImports, theFunctionDeclaration, theFunctionCalls].join("\n\n")
+    );
+  } catch (error) {
+    console.error("Failed to write register-functions.js");
+    console.error(error);
+    process.exit(1);
+  }
+}
 
 main();
--- src/backend/react-docgen.d.ts
+++ src/backend/react-docgen.d.ts
@@ -1,39 +1,41 @@
 type Result = {
   description: string;
   displayName: string;
-  props: { [propName: string]: { 
-    type: {
-      name: string;
-      value?: string;
-      raw?: string;
-    }
-    flowType?: object;
-    tsType?: object;
-    required: boolean;
-    description: string;
-    defaultValue?: {
-      value: string;
-      computed: boolean;
-    }
-  }};
+  props: {
+    [propName: string]: {
+      type: {
+        name: string;
+        value?: string;
+        raw?: string;
+      };
+      flowType?: object;
+      tsType?: object;
+      required: boolean;
+      description: string;
+      defaultValue?: {
+        value: string;
+        computed: boolean;
+      };
+    };
+  };
   composes?: string;
-}
+};
 
-declare module 'react-docgen' {
+declare module "react-docgen" {
   export function parse(
-    source: string | Buffer, 
+    source: string | Buffer,
     resolver?: (...args: any) => any,
     handlers?: (...args: any) => void,
     options?: {
       fileName?: string;
       cwd?: string;
       parserOptions?: object;
-      resolver?: (...args: any) => any,
-      handlers?: (...args: any) => any,
+      resolver?: (...args: any) => any;
+      handlers?: (...args: any) => any;
     }
-  ): Result
+  ): Result[];
 
   export const resolver: {
-    findAllExportedComponentDefinitions: (...args: any) => any; 
-  }
+    findAllExportedComponentDefinitions: (...args: any) => any;
+  };
 }
